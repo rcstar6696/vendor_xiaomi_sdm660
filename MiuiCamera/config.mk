905c590afecafd9a9d29301b82c78f155b688c58
Max Weffers <rcstar6696@gmail.com>
2018-10-18 18:00:52 +0200

    sdm660: miuicam: Set correct path

--- MiuiCamera/config.mk
+++ MiuiCamera/config.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-VENDOR_PATH := vendor/xiaomi/MiuiCamera
+VENDOR_PATH := vendor/xiaomi/sdm660/MiuiCamera
 
 PRODUCT_COPY_FILES += \
     $(VENDOR_PATH)/system/etc/device_features/wayne.xml:system/etc/device_features/wayne.xml \
